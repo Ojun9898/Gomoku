9db0c56ccbdec0f994521e2a90ec6c501c348c5d
OJUN <dhwns0703@gmail.com>
2025-03-12 09:22:13 +0900

    스크립트 생성
    스크립트 생성

---
+++ Assets/Scripts/Game/Map/Buff.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff : IMapEffect
+{
+    public void On()
+    {
+    }
+
+    public void Off()
+    {
+    }
+}
